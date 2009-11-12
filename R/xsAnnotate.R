3b2fd2f40aec30b47222a6c6ed4d6d93199376f8
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-11-12 14:09:04 +0000

    Dont create factors when reading internal ruleses
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@42931 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xsAnnotate.R
+++ R/xsAnnotate.R
@@ -732,12 +732,12 @@ calcRules <- function (maxcharge=3,mol=3,nion=2,nnloss=1,nnadd=1,nh=2,polarity=N
     ionlist <- system.file('lists/ions.csv', package = "CAMERA")[1]
     if (!file.exists(ionlist)) stop('ionlist.csv not found.')
     ionlist<-read.table(ionlist, header=TRUE, dec=".", sep=",",
-                        as.is=TRUE, stringsAsFactors = TRUE);
+                        as.is=TRUE, stringsAsFactors = FALSE);
 
     neutralloss <- system.file('lists/neutralloss.csv', package = "CAMERA")[1]
     if (!file.exists(neutralloss)) stop('neutralloss.csv not found.')
     neutralloss <- read.table(neutralloss, header=TRUE, dec=".", sep=",",
-                              as.is=TRUE, stringsAsFactors = TRUE);
+                              as.is=TRUE, stringsAsFactors = FALSE);
 
     neutraladdition <- system.file('lists/neutraladdition.csv', package = "CAMERA")[1]
     if (!file.exists(neutraladdition)) stop('neutraladdition.csv not found.')
