1792ff5730cecb91897d7c40a6be2112a9cba491
c.kuhl <c.kuhl@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-03-24 16:04:32 +0000

    blos der fix mit dem length(pspectra)+1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@45466 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.groupFWHM.R
+++ inst/unitTests/runit.groupFWHM.R
@@ -1,25 +1,48 @@
 ## single sample
 test.groupFWHM_CORR <- function() {
- library(faahKO)         
- file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
- xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
- an   <- xsAnnotate(xs)
- anF   <- groupFWHM(an)
- checkEqualsNumeric(nrow(xs@peaks),126)
- checkEqualsNumeric(length(an@pspectra),14)
- checkEqualsNumeric(an@pspectra[[5]][5],86)
- anC <- groupCorr(an) ## check to be continued tomorrow here
-}
+    file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
+    xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
+    an   <- xsAnnotate(xs)
+    anF   <- groupFWHM(an)
+    checkEqualsNumeric(nrow(xs@peaks),126)
+    checkEqualsNumeric(length(anF@pspectra),14)
+    checkEqualsNumeric(anF@pspectra[[5]][5],86)
+    anC <- groupCorr(an) ## groupCORR without groupFWHM
+    checkEqualsNumeric(length(anC@pspectra),55)
+    anFC <- groupCorr(anF) ## groupCORR with groupFWHM
+    checkEqualsNumeric(length(anFC@pspectra),43)
 
+ ## groupCorr with findIsotopes before
+    anI <- findIsotopes(an)
+    anIC <- groupCorr(anI)
+    }
 
-library(faahKO)         
-filepath <- system.file("cdf", package = "faahKO")
-xsg <- group(faahko)
-## manual selection
-xsa <- xsAnnotate(xsg, sample=1)
-xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
-xsaC <- groupCorr(xsaF)
-## highestPeak-selection
-xsa <- xsAnnotate(xsg, sample=NA)
-xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
-xsaC <- groupCorr(xsaF)
+test.groupFWHM_CORR_multi <- function() {
+    library(faahKO)         
+    filepath <- system.file("cdf", package = "faahKO")
+    xsg <- group(faahko)
+  ##  groupCorr after groupFWHM
+    ## manual selection
+    xsa <- xsAnnotate(xsg, sample=1)
+    xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+    xsaC <- groupCorr(xsaF)
+    checkEqualsNumeric(length(xsaC@pspectra),275)
+    ## highestPeak-selection
+    xsa <- xsAnnotate(xsg, sample=NA)
+    xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+    xsaC <- groupCorr(xsaF)
+    checkEqualsNumeric(length(xsaC@pspectra),344)
+ ## groupCorr with polarity = "negative"
+    xsaCN <- groupCorr(xsaF,polarity="negative")
+    checkEqualsNumeric(length(xsaCN@pspectra),344)
+
+  ##  groupCorr without groupFWHM
+    ## manual selection
+    xsa <- xsAnnotate(xsg, sample=1)
+    xsaC <- groupCorr(xsa)
+    checkEqualsNumeric(length(xsaC@pspectra),353)
+    ## highestPeak-selection
+    xsa <- xsAnnotate(xsg, sample=NA)
+    xsaC <- groupCorr(xsa)
+    ## checkEqualsNumeric(length(xsaC@pspectra),344) --> erzeugt blos ein pspectrum .. wieso?
+    }
