653cb8fc82dafa334ffedcc7e926e6e29ea22bc4
c.kuhl <c.kuhl@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-03-24 16:04:05 +0000

    current working at this
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@45462 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.groupFWHM.R
+++ inst/unitTests/runit.groupFWHM.R
@@ -1,9 +1,25 @@
-test.groupFWHM <- function() {
+## single sample
+test.groupFWHM_CORR <- function() {
+ library(faahKO)         
  file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
  xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
  an   <- xsAnnotate(xs)
- an   <- groupFWHM(an)
+ anF   <- groupFWHM(an)
  checkEqualsNumeric(nrow(xs@peaks),126)
  checkEqualsNumeric(length(an@pspectra),14)
  checkEqualsNumeric(an@pspectra[[5]][5],86)
+ anC <- groupCorr(an) ## check to be continued tomorrow here
 }
+
+
+library(faahKO)         
+filepath <- system.file("cdf", package = "faahKO")
+xsg <- group(faahko)
+## manual selection
+xsa <- xsAnnotate(xsg, sample=1)
+xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+xsaC <- groupCorr(xsaF)
+## highestPeak-selection
+xsa <- xsAnnotate(xsg, sample=NA)
+xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+xsaC <- groupCorr(xsaF)
