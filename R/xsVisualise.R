f3695f4c39c64cac89cb9b45b9f51f54341344c7
Carsten Kuhl <ckuhl@ipb-halle.de>
2010-04-14 15:15:57 +0000

    optimized source of plotEIC
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@46003 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xsVisualise.R
+++ R/xsVisualise.R
@@ -1,103 +1,127 @@
 setGeneric("plotEICs", function(object,
-                                xraw,
                                 pspecIdx=1:length(object@pspectra),
                                 maxlabel=0, sleep=0,
                                 ...) standardGeneric("plotEICs"))
 setMethod("plotEICs", "xsAnnotate", function(object,
-                                             xraw,
                                              pspecIdx=1:length(object@pspectra),
-                                             maxlabel=0, sleep=0)
-      {
+                                             maxlabel=0, sleep=0){
+           #stop("angst oO")
+           smpls <- unique(object@psSamples[pspecIdx])
+           #Ranges <- matrix(ncol=4, nrow=length(pspecIdx))
+           xeic <- new("xcmsEIC")
+           xeic@rtrange <- matrix(nrow=length(pspecIdx), ncol=2)
+           xeic@mzrange <- matrix(nrow=length(pspecIdx), ncol=2)
+           pcpos <- 1
+           for (a in 1:length(smpls)) { ## sample-wise EIC collection
+               xraw <- xcmsRaw(object@xcmsSet@filepaths[smpls[a]])
+               rtmargin <- 0.018 * max(xraw@scantime) 
+               pspecS <- pspecIdx[which(object@psSamples[pspecIdx] == smpls[a])]
+               peaks <- CAMERA:::getPeaks(object@xcmsSet,smpls[a])  ## getting ALL peaks from the current sample (not that bad)
+               eic <- lapply (pspecS, function(pc) {
+                   pidx <- object@pspectra[[pc]]
+                   pks <- peaks[pidx,]
+                   gks <- object@groupInfo[pidx,]
+                   nap <- which(is.na(pks[,1]))
+                   pks[nap,] <- cbind(gks[nap,c(1:6), drop=FALSE],matrix(nrow=length(nap),ncol=5,0))
+                   bbox <- c(rtmin = min(pks[,"rtmin"])-rtmargin,
+                             rtmax = max(pks[,"rtmax"])+rtmargin,
+                             mzmin = min(pks[,"mzmin"]),
+                             mzmax = max(pks[,"mzmax"]))
+                   eic <- xcms:::getEIC(xraw,
+                                 rtrange=matrix(bbox[c("rtmin","rtmax")],
+                                 nrow=length(pidx), ncol=2, byrow=TRUE),
+                                 mzrange=pks[,c("mzmin", "mzmax"), drop=FALSE])
+                   xeic@rtrange[pcpos, ] <<- bbox[c("rtmin","rtmax")]
+                   xeic@mzrange[pcpos, ] <<- bbox[c("mzmin","mzmax")]
+                   cat("-->",pcpos,"\n")
+                   pcpos <<- pcpos+1
+                    ##stop("WTF?")
+                   eic@eic[[1]]
+                   })
+               xeic@eic <- c(xeic@eic, eic)
+            }
+           names(xeic@eic) <- paste("Pseudospectrum ", pspecIdx, sep="")
 
-          ## Expand RT plot width by
-          ## 1% of runtime:
-          rtmargin <- 0.01 * max(xraw@scantime)
+  ## Setup Peak and Neighbourhood Colors,
+  ## Now: all black
+  ## Later: by Annotation 
+  ##
 
-          ##
-          ## Calculate EICs of requested
-          ## PC groups
-          ##
-          xeic <- new("xcmsEIC")
-          xeic@rtrange <- matrix(nrow=length(pspecIdx), ncol=2)
-          xeic@mzrange <- matrix(nrow=length(pspecIdx), ncol=2)
-
-          pcpos <- 1
-          xeic@eic <- lapply (pspecIdx, function(pc) {
-              pidx <- object@pspectra[[pc]]
-              bbox <- c(rtmin = min(object@peaks[pidx,"rtmin"])-rtmargin,
-                        rtmax = max(object@peaks[pidx,"rtmax"])+rtmargin,
-                        mzmin = min(object@peaks[pidx,"mzmin"]),
-                        mzmax = max(object@peaks[pidx,"mzmax"]))
-              eic <- xcms:::getEIC(xraw,
-                            rtrange=matrix(bbox[c("rtmin","rtmax")],
-                            nrow=length(pidx), ncol=2, byrow=TRUE),
-                            mzrange=object@peaks[pidx,c("mzmin", "mzmax"), drop=FALSE])
-
-              xeic@rtrange[pcpos, ] <<- bbox[c("rtmin","rtmax")]
-              xeic@mzrange[pcpos, ] <<- bbox[c("mzmin","mzmax")]
-              pcpos <<- pcpos+1
-              eic@eic[[1]]
-          })
-          names(xeic@eic) <- paste("Pseudospectrum ", pspecIdx, sep="")
-
-          ##
-          ## Setup Peak and Neighbourhood Colors,
-          ## Now: all black
-          ## Later: by Annotation ?
-          ##
-          lcol <- 1
-          for (i in seq(along = lcol)) {
-              rgbvec <- pmin(col2rgb(lcol[i])+153,255)
-              lcol[i] <- rgb(rgbvec[1], rgbvec[2], rgbvec[3], max = 255)
-          }
-
-          ##
-          ## Loop through all pspectra
-          ##
-          peaks <- object@peaks
-          for (pspec in 1:length(xeic@eic)) {
-              ## Calculate EICs and plot ranges
-              neics <- length(xeic@eic[[pspec]])
-              lmaxlabel <- min(maxlabel, neics)
-              col <- c((lmaxlabel+1):2, rep(1, neics-lmaxlabel))
-              eicidx <- 1:neics
-
-              maxint <- numeric(length(eicidx))
-              for (j in eicidx) {
-                  maxint[j] <- max(xeic@eic[[pspec]][[j]][,"intensity"])
+ ## creating a lightcolor-vector:
+  rgbcol <- matrix(nrow=25,ncol=3) ## generating 25 rgb-values
+  colv <- c(0,0.6,1) ## color-intensities
+  id<-c(1,1,1)
+  for (a in 1:25){
+      rgbcol[a,] <- c(colv[id[1]],colv[id[2]],colv[id[3]])
+      if (id[3] < 3) {
+          id[3] <- id[3] + 1
+          }else{
+          id[3] <- 1
+          if (id[2] < 3) {
+              id[2] <- id[2] + 1
+              }else{ 
+              id[2] <- 1
+              id[1] <- id[1] + 1
               }
-              rtrange <- xeic@rtrange[pspec, ]
-
-              ## Open Plot
-              plot(0, 0, type = "n", xlim = rtrange, ylim = c(0, max(maxint)),
+          }
+    } ## kind of binary-addition loop
+  rgbcol <- rgbcol[order(apply(rgbcol,1,sum)),]
+  col <- rgb(rgbcol[-14,]) ## remove middle-grey and generating colors
+  lcol <- rgb(0.6,0.6,0.6)
+  ##
+  ## Loop through all pspectra
+  ##
+   # stop("again")
+  for (pspec in 1:length(pspecIdx)) {
+    EIC<-xeic@eic[[pspec]]
+    pidx <- object@pspectra[[pspecIdx[pspec]]]    
+    peaks <- CAMERA:::getPeaks(object@xcmsSet,object@psSamples[pspecIdx[pspec]])[pidx,]
+    grps <- object@groupInfo[pidx,]
+    nap <- which(is.na(peaks[,1]))
+    naps <- rep(FALSE, nrow(peaks))
+    naps[nap] <- TRUE
+    peaks[nap,] <- cbind(grps[nap,c(1:6), drop=FALSE],matrix(nrow=length(nap),ncol=5,0))
+    main <- paste("Pseudospectrum ", pspecIdx[pspec], sep="")
+    ## Calculate EICs and plot ranges
+    neics <- length(pidx)
+    lmaxlabel <- min(maxlabel, neics)
+    ##col <- c((lmaxlabel+1):2, rep(1, neics-lmaxlabel))
+    eicidx <- 1:neics;
+    maxint <- numeric(length(eicidx))
+    for (j in eicidx) {
+        maxint[j] <- max(EIC[[j]][,"intensity"])
+    }
+    rt <- xeic@rtrange[pspec, ];
+    ## Open Plot
+   # stop("testing idx 4,5")
+    plot(0, 0, type = "n", xlim = rt, ylim = c(0, max(maxint)),
                    xlab = "Retention Time (seconds)", ylab = "Intensity",
-                   main = paste("Extracted Ion Chromatograms for ", names(xeic@eic)[pspec]))
+                   main = paste("Extracted Ion Chromatograms for ", main))
+    ## Plot Peak and surrounding
+    o <- order(maxint, decreasing = TRUE)
+    for (j in eicidx[o]) {
+        pts <- xeic@eic[[pspec]][[j]]
+        points(pts, type = "l", col = lcol)
+        peakrange <- peaks[,c("rtmin","rtmax"), drop=FALSE]
+        ptsidx <- pts[,"rt"] >= peakrange[j,1] & pts[,"rt"] <= peakrange[j,2]
+        if (naps[j]) points(pts[ptsidx,], type = "l", col = col[j], lwd=1.3, lty=3) else
+                     points(pts[ptsidx,], type = "l", col = col[j], lwd=1.3)
+    }
+    ## Plot Annotation Legend
+    pspectrum <- getpspectra(object, grp=pspecIdx[pspec])
+    if (lmaxlabel>0 & "adduct" %in% colnames(pspectrum)) {
+      adduct <- sapply(strsplit(pspectrum[o[1:lmaxlabel], "adduct"], " "),
+                       function(x) {if (length(x)>0) x[1] else ""})
+      mz <- format(pspectrum[o[1:lmaxlabel], "mz"], digits=5)
+      legend("topright", legend=paste(mz, adduct),
+             col=col[o], lty=1)
+    }
+    if (sleep > 0) {
+      Sys.sleep(sleep)
+    }
+  }
+})
 
-              ## Plot Peak and surrounding
-              o <- order(maxint, decreasing = TRUE)
-              col[o] <- col
-              for (j in eicidx[o]) {
-                  pts <- xeic@eic[[pspec]][[j]]
-                  points(pts, type = "l", col = lcol[j])
-                  peakrange <- peaks[object@pspectra[[pspecIdx[pspec]]], c("rtmin","rtmax"), drop=FALSE]
-                  ptsidx <- pts[,"rt"] >= peakrange[j,1] & pts[,"rt"] <= peakrange[j,2]
-                  points(pts[ptsidx,], type = "l", col = col[j])
-              }
-
-              ## Plot Annotation Legend
-              pspectrum <- getpspectra(object, grp=pspecIdx[pspec])
-              if (lmaxlabel>0 & "adduct" %in% colnames(pspectrum)) {
-                adduct <- sapply(strsplit(pspectrum[o[1:lmaxlabel], "adduct"], " "),
-                                 function(x) {if (length(x)>0) x[1] else ""})
-                mz <- format(pspectrum[o[1:lmaxlabel], "mz"], digits=5)
-                legend("topright", legend=paste(mz, adduct),
-                       col=col[o], lty=1)
-              }
-
-              if (sleep > 0)
-                  Sys.sleep(sleep)
-          }
-   })
 
 
 setGeneric("plotPsSpectrum", function(object, pspec=1:length(object@pspectra), log=FALSE,
