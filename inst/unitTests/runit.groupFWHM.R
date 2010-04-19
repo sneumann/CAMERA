03026da3ac46f2134aef7da9732b88135187d84a
c.kuhl <c.kuhl@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-04-19 08:53:50 +0000

    blos der fix mit dem length(pspectra)+1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@46202 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.groupFWHM.R
+++ inst/unitTests/runit.groupFWHM.R
@@ -66,3 +66,49 @@ test.anno_multi <- function() {
     }
 
 
+    anIC <- groupCorr(anI)
+    checkEqualsNumeric(length(anIC@pspectra),35)
+ ## groupCorr with polarity = "negative"
+    anCN <- groupCorr(anF,polarity="negative")
+    checkEqualsNumeric(length(anCN@pspectra),41)
+ ## findIsotopes and findAdducts
+    
+    anFI <- findIsotopes(anFC)
+    checkEqualsNumeric(nrow(anFI@isoID),27)
+    anFA <- findAdducts(anFI, polarity="positive")
+    checkEqualsNumeric(length(unique(anFA@annoID[,1])),33)
+    ## findAdducts with psg_list
+    anFAc <- findAdducts(anFI, polarity="positive", psg_list=c(5,6,7,8,9,10,11,12))
+    checkEqualsNumeric(length(unique(anFAc@annoID[,1])),8)
+    }
+
+test.anno_multi <- function() {
+    library(faahKO)         
+    filepath <- system.file("cdf", package = "faahKO")
+    xsg <- group(faahko)
+  ##  groupCorr after groupFWHM
+    ## manual selection
+    xsa <- xsAnnotate(xsg, sample=1)
+    xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+    xsaC <- groupCorr(xsaF)
+    checkEqualsNumeric(length(xsaC@pspectra),275)
+    ## highestPeak-selection
+    xsa <- xsAnnotate(xsg, sample=NA)
+    xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
+    xsaC <- groupCorr(xsaF)
+    checkEqualsNumeric(length(xsaC@pspectra),213)
+  ##  groupCorr without groupFWHM
+    ## manual selection
+    xsa <- xsAnnotate(xsg, sample=1)
+    xsaC <- groupCorr(xsa)
+    checkEqualsNumeric(length(xsaC@pspectra),353)
+    ## highestPeak-selection
+    xsa <- xsAnnotate(xsg, sample=NA)
+    xsaC <- groupCorr(xsa)
+    checkEqualsNumeric(length(xsaC@pspectra),353)
+ ## findIsotopes and findAdducts
+    xsaFI <- findIsotopes(xsaC)
+    checkEqualsNumeric(nrow(xsaFI@isoID),14)
+    xsaFA <- findAdducts(xsaFI, polarity="positive")
+    checkEqualsNumeric(length(unique(xsaFA@annoID[,1])),23)
+    }
