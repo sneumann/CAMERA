36974db17392a3f94494af67234492d7cfafc1cd
Carsten Kuhl <ckuhl@ipb-halle.de>
2011-03-31 14:55:55 +0000

    Bump version number to 1.8 Changed S4 Header of calcCaS and CalsCiS Fix Bug in calcCaS if xsAnnotate object has not been grouped before Adopted Unit tests Fix findIsotopes, if NA occurs and only subsets or one sample has been seleted
    Signed-off-by: ckuhl <ckuhl@vogt.ipb-sub.ipb-halle.de>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@54309 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.groupFWHM.R
+++ inst/unitTests/runit.groupFWHM.R
@@ -2,33 +2,51 @@
 test.anno_single <- function() {
     file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
     xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
+
     an   <- xsAnnotate(xs)
+
     anF   <- groupFWHM(an)
+
     checkEqualsNumeric(nrow(xs@peaks),126)
+
     checkEqualsNumeric(length(anF@pspectra),14)
+
     checkEqualsNumeric(anF@pspectra[[5]][5],86)
- ## groupCORR without groupFWHM
+
+    ## groupCORR without groupFWHM
     anC <- groupCorr(an) 
     checkEqualsNumeric(length(anC@pspectra),48)
- ## groupCORR with groupFWHM
+
+    ## groupCORR with groupFWHM
     anFC <- groupCorr(anF) 
     checkEqualsNumeric(length(anFC@pspectra),48)
+
     ## groupCorr with  psg_list
-         anFCp <- groupCorr(anF, psg_list=c(5,6,7,8,9,10,11,12)) 
+    anFCp <- groupCorr(anF, psg_list=c(5,6,7,8,9,10,11,12)) 
         
- ## groupCorr with findIsotopes before
+    ## groupCorr with findIsotopes before
     anI <- findIsotopes(anF)
     anIC <- groupCorr(anI)
     checkEqualsNumeric(length(anIC@pspectra),45)
- ## groupCorr with polarity = "negative"
+
+    ## groupCorr with polarity = "negative"
     anCN <- groupCorr(anF,polarity="negative")
     checkEqualsNumeric(length(anCN@pspectra),48)
- ## findIsotopes and findAdducts
-    
+
+    ## findIsotopes without group before
+    anI2 <- findIsotopes(an)
+    checkEqualsNumeric(nrow(anFI@isoID),26)
+
+    ## findAdducts without anything before
+    anA <- findAdducts(an, polarity="positive")
+    checkEqualsNumeric(length(unique(anA@annoID[,1])),77)
+
+    ## findIsotopes and findAdducts 
     anFI <- findIsotopes(anFC)
     checkEqualsNumeric(nrow(anFI@isoID),26)
     anFA <- findAdducts(anFI, polarity="positive")
     checkEqualsNumeric(length(unique(anFA@annoID[,1])),40)
+
     ## findAdducts with psg_list
     anFAc <- findAdducts(anFI, polarity="positive", psg_list=c(5,6,7,8,9,10,11,12))
     checkEqualsNumeric(length(unique(anFAc@annoID[,1])),9)
@@ -38,19 +56,19 @@ test.anno_multi <- function() {
     library(faahKO)         
     filepath <- system.file("cdf", package = "faahKO")
     xsg <- group(faahko)
-  ##  groupCorr after groupFWHM
+    ##  groupCorr after groupFWHM
     ## manual selection
     xsa <- xsAnnotate(xsg, sample=1)
     xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
     xsaC <- groupCorr(xsaF)
-    #Change from 171 to 159 due to applying correlation accros samples
+    #Change from 171 to 159 due to applying correlation accross samples
     checkEqualsNumeric(length(xsaC@pspectra),159)
     #checkEqualsNumeric(length(xsaC@pspectra),171)
     ## highestPeak-selection
     xsa <- xsAnnotate(xsg, sample=NA)
     xsaF <- groupFWHM(xsa, sigma=6, perfwhm=0.6)
     xsaC <- groupCorr(xsaF)
-    #Change from 211 to 236 due to applying correlation accros samples
+    #Change from 211 to 236 due to applying correlation accross samples
     checkEqualsNumeric(length(xsaC@pspectra),236)
     #checkEqualsNumeric(length(xsaC@pspectra),211)
   ##  groupCorr without groupFWHM
@@ -71,8 +89,10 @@ test.anno_multi <- function() {
     #Change from 20 to 109 due to applying correlation accros samples
     #checkEqualsNumeric(nrow(xsaFI@isoID),109)
     #checkEqualsNumeric(nrow(xsaFI@isoID),20)
-    xsaFA <- findAdducts(xsaFI, polarity="positive")
+    file  <- system.file('rules/primary_adducts_pos.csv', package = "CAMERA")
+    rules <- read.csv(file)
+    xsaFA <- findAdducts(xsaFI, polarity="positive",rules=rules)
     #Change from 41 to 154 due to applying correlation accros samples
-    checkEqualsNumeric(length(unique(xsaFA@annoID[,1])),154)
+    checkEqualsNumeric(length(unique(xsaFA@annoID[,1])),151)
     #checkEqualsNumeric(length(unique(xsaFA@annoID[,1])),41)
     }
