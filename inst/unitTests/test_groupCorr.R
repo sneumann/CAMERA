3a8a998c4aadfdfefc7c0c1470b87cb6877d24e5
Carsten Kuhl <ckuhl@ipb-halle.de>
2013-01-23 17:04:24 +0000

    Some changes in the unitTests Renamed all unitTests Add man pages for groupDen, findKendrickMasses and plotPeakEICs
    Signed-off-by: Carsten Kuhl <ckuhl@schur.ipb-sub.ipb-halle.de>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@72815 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_groupCorr.R
@@ -0,0 +1,46 @@
+library(CAMERA)
+
+test.grp.lpc <- function() {
+    file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
+    xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
+
+    an   <- xsAnnotate(xs)
+
+    anF   <- groupFWHM(an)
+
+    checkEqualsNumeric(nrow(xs@peaks),126,"xset contains 126 Peaks")
+
+    checkEqualsNumeric(length(anF@pspectra),14,"xsa has 14 compound spectra")
+
+    checkEqualsNumeric(anF@pspectra[[5]][5],86,
+                       "5th peak in 5th compound spectra is number 86")
+
+    ## groupCORR without groupFWHM
+    anC <- groupCorr(an,graphMethod="lpc") 
+    checkEqualsNumeric(length(anC@pspectra),18,
+                       "lpc created 17 compound spectra")
+
+    ## groupCORR with groupFWHM
+    
+    anFC <- groupCorr(anF,graphMethod="lpc") 
+    checkEqualsNumeric(length(anFC@pspectra),35,
+                       "lpc created 35 compound spectra")
+    
+    }
+
+test.grpDen <- function() {
+  file <- system.file('mzdata/MM14.mzdata', package = "CAMERA")
+  xs   <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5,10))
+  
+  xsa   <- xsAnnotate(xs)
+  
+  xsa.grp   <- groupDen(xsa, bw=0.4)
+  
+  checkEqualsNumeric(nrow(xs@peaks),126)
+  
+  checkEqualsNumeric(length(xsa.grp@pspectra),11)
+  
+  checkEqualsNumeric(xsa.grp@pspectra[[5]][5],100)
+  
+}
+
