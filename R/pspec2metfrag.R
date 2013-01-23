9b595f640f589408e757df845a9748cb16e6f292
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-01-23 21:56:40 +0000

    added pspec2metfusion
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@72836 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pspec2metfrag.R
+++ R/pspec2metfrag.R
@@ -1,4 +1,4 @@
-pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
+extractfragments <- function(object, pspecidx=NULL) {
 
   if(class(object) != "xsAnnotate"){
     stop("Object parameter is no xsAnnotate object.\n");
@@ -45,9 +45,9 @@ pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
 
       ## All peaks in sp annotated with *this* annoid
       annopeakidx <- object@pspectra[[pspec]] %in% object@annoID[
-        which(object@annoID[, "grp_id"]==annoid & (object@annoID[,"rule_id"] %in%(ruleLoss))), "id"]
+        which(object@annoID[, "grpID"]==annoid & (object@annoID[,"ruleID"] %in%(ruleLoss))), "id"]
       fragmentidx <- object@pspectra[[pspec]] %in% object@annoID[
-	which(object@annoID[, "grp_id"]==annoid & !(object@annoID[,"rule_id"] %in%(ruleLoss))), "id"]
+	which(object@annoID[, "grpID"]==annoid & !(object@annoID[,"ruleID"] %in%(ruleLoss))), "id"]
 	annopeakidx <- annopeakidx & !fragmentidx
       ## unannotated OR monoisotopic peaks
       nonisoidx <- sapply(peakID, function(x) {
@@ -60,7 +60,7 @@ pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
       #nonisoidx <- grepl("(^$)|( 0 +)", sp[, "isotopes"])
 
       ## all known-to-be-multiply-charges
-      multichargeidx <- object@pspectra[[pspec]] %in% sapply(object@annoID[object@annoID[, "grp_id"]==annoid, "id"], 
+      multichargeidx <- object@pspectra[[pspec]] %in% sapply(object@annoID[object@annoID[, "grpID"]==annoid, "id"], 
                                                          function(x) {
                                                            ion <- object@derivativeIons[[x]];
                                                            if (length(ion) > 0) { 
@@ -78,9 +78,15 @@ pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
       o <- order(filteredsp[, "mz"])
       if (length(o) > 0) {
         result[[length(result)+1]] <- list(Pseudospectrum=pspec, AnnotationID=annoid, RentionTime=rt, 
-                                           ParentMass=neutralmass, Peaks=filteredsp[o, c("mz", intIdx), drop=FALSE])    }      
+                                           ParentMass=neutralmass, Peaks=filteredsp[o, c("mz", intIdx), drop=FALSE])
+      }
     }
   }
+  invisible(result)
+}  
+
+
+fragments2metfrag <- function(result, filedir=NULL) {
   if(is.null(filedir)){
     return(invisible(result))
   }else{
@@ -96,5 +102,50 @@ pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
       })
     return(NULL);
   }
+}
+
+pspec2metfrag <- function(object, pspecidx=NULL, filedir=NULL) {
+  result <- extractfragments(object, pspecidx) 
+  fragments2metfrag(result, filedir)
 }  
 
+## CAMERA pseudo-spectra to MetFusion
+
+pspec2metfusion <- function(object, pspecidx=NULL, filedir=NULL) {
+  result <- extractfragments(object, pspecidx) 
+  fragments2metfusion(result, filedir)
+}
+
+fragments2metfusion <- function(result, filedir=NULL) {
+  if(is.null(filedir)){
+    return(invisible(result))
+  }else{
+    lapply(result, function(x){
+      zz <- file(paste(filedir, x$Pseudospectrum, "_", x$AnnotationID, ".mf", sep=""), "w");
+      cat("# mfDatabaseIDs: \n", file=zz)
+      cat("# mfLimit: 10000", "\n", file=zz)
+      cat("# mfFormula: ", "\n", file=zz)
+      cat("# mbLimit: 100", "\n", file=zz)
+      cat("# mfDatabase: ", "chemspider", "\n", file=zz)    # kegg, pubchem, chemspider, beilstein, knapsack, massbank, sdf
+      cat("# mfMZabs: 0.01", "\n", file=zz)
+      cat("# mfMZppm: 30.0", "\n", file=zz)
+      cat("# clustering: true", "\n", file=zz)
+      cat("# mbInstruments: CE-ESI-TOF,ESI-IT-MS/MS,ESI-QqIT-MS/MS,ESI-QqQ-MS/MS,ESI-QqTOF-MS/MS,LC-ESI-IT,LC-ESI-ITFTLC-ESI-ITTOF,LC-ESI-Q,LC-ESI-QIT,LC-ESI-QQ,LC-ESI-QTOF", "\n", file=zz)
+      cat("# mbCutoff: 5", "\n", file=zz)
+      cat("# mfAdduct: ", "Neutral", "\n", file=zz)
+      cat("# mfSearchPPM: 30.0", "\n", file=zz)
+      cat("# mfParentIon: ", x$ParentMass, "\n", file=zz)
+      cat("# mbIonization: ", substr(object@polarity,1,3), "\n", file=zz)  # pos, neg, both
+      cat("# mfExactMass: ", x$ParentMass, "\n", file=zz)
+      #cat("# Mode: 1\n", file=zz)
+      #cat("# Pseudospectrum:", x$Pseudospectrum, "\n", file=zz)
+      #cat("# Annotation alternative:", x$AnnotationID, "\n", file=zz)
+      #cat("# Retentiontime:", x$RentionTime, "\n", file=zz)
+      #cat("# Parent Mass: ", x$ParentMass, "\n", file=zz)
+      cat("# peaks: ", "\n", file=zz)
+      write.table(x$Peaks,row.names=FALSE, col.names=FALSE, file=zz)
+      close(zz)
+    })
+    return(NULL);
+  }
+}  
--- inst/unitTests/runit.pspec2metfrag.R
+++ inst/unitTests/runit.pspec2metfrag.R
@@ -1,11 +1,11 @@
-test.psp3c2metfrag_single <- function() {
+test.psp3c2metfrag <- function() {
 
-#file        <- system.file('mzdata/MM14.mzdata', package = "CAMERA");
-#xs          <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5, 10));
-#an          <- xsAnnotate(xs);
-#an.group    <- groupFWHM(an);
-#an.iso      <- findIsotopes(an.group); #optional step
-#an.adducts  <- findAdducts(an.corr)
+file        <- system.file('mzdata/MM14.mzdata', package = "CAMERA");
+xs          <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5, 10));
+an          <- xsAnnotate(xs);
+an          <- groupFWHM(an);
+an          <- findIsotopes(an); #optional step
+an          <- findAdducts(an, polarity="positive")
 
 ##
 ## Example call for three pspec
@@ -13,21 +13,15 @@ test.psp3c2metfrag_single <- function() {
 ##       2) add mode + ppm etc.
 ##
 
-#pspec2metfrag(an.adducts, pspecidx=c(3))
+pspec2metfrag(an, pspecidx=c(3))
 
-#pspec2metfrag(an.adducts, pspecidx=c(7))
+pspec2metfrag(an, pspecidx=c(7))
 
-#pspec2metfrag(an.adducts, pspecidx=c(1,3))
+pspec2metfusion(an, pspecidx=c(1,3))
 
-#pspec2metfrag(an.adducts, pspecidx=c(2))
+pspec2metfrag(an, pspecidx=c(2))
 
-#pspec2metfrag(an.adducts)
-
-
-
-#pspec <- 3
-#xa <- an.adducts
-#i <- 1
+pspec2metfrag(an)
 
 }
 
