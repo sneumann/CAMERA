6391662ed042682873793c21afcdf4b0bd50279c
Steffen Neumann <sneumann@ipb-halle.de>
2013-01-23 21:56:25 +0000

    Camera2Metfrag export started
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CAMERA@72835 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/runit.pspec2metfrag.R
@@ -0,0 +1,33 @@
+test.psp3c2metfrag_single <- function() {
+
+#file        <- system.file('mzdata/MM14.mzdata', package = "CAMERA");
+#xs          <- xcmsSet(file, method="centWave", ppm=30, peakwidth=c(5, 10));
+#an          <- xsAnnotate(xs);
+#an.group    <- groupFWHM(an);
+#an.iso      <- findIsotopes(an.group); #optional step
+#an.adducts  <- findAdducts(an.corr)
+
+##
+## Example call for three pspec
+## TODO: 1) guess precursor
+##       2) add mode + ppm etc.
+##
+
+#pspec2metfrag(an.adducts, pspecidx=c(3))
+
+#pspec2metfrag(an.adducts, pspecidx=c(7))
+
+#pspec2metfrag(an.adducts, pspecidx=c(1,3))
+
+#pspec2metfrag(an.adducts, pspecidx=c(2))
+
+#pspec2metfrag(an.adducts)
+
+
+
+#pspec <- 3
+#xa <- an.adducts
+#i <- 1
+
+}
+
